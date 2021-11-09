a251923fca1c0444090a70023a65170473b577d9
knight3001 <knight3001@gmail.com>
2021-11-09 15:21:55 +1000

    1 file modified

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { useParams } from 'react-router-dom';
-import { getInvoice } from '../data';
+
+import { useParams, useNavigate } from 'react-router-dom';
+import { getInvoice, deleteInvoice } from '../data';
 
 export default function Invoice() {
+  let navigate = useNavigate();
   let params = useParams();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
+
   return (
     <main style={{ padding: '1rem' }}>
       <h2>Total Due: {invoice.amount}</h2>
@@ -12,6 +15,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate('/invoices');
+          }}
+        >
+          Delete
+        </button>
+      </p>
     </main>
   );
 }
